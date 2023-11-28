0275f03c509a23e71e8f7ddb6a184e3c2860de76
rubycidev <117899216+rubycidev@users.noreply.github.com>
2023-11-28 16:22:48 +0100

    Update rails_helper.rb to load .env file

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,6 +6,8 @@ require File.expand_path("../../config/environment", __FILE__)
 require "rspec/rails"
 require "simplecov"
 require "super_diff/rspec-rails"
+require 'dotenv/load'
+Dotenv.load
 
 SimpleCov.start "rails"
 # Requires supporting ruby files with custom matchers and macros, etc,
