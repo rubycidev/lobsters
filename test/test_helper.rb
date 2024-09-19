a93d834a556db07b51773f79289cb468337d69aa
SasaV-Kolosek <sasavas@kolosek.com>
2024-09-19 14:06:37 +0200

    load minitest

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,6 +4,11 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
+# Load Minitest framework
+require 'minitest/reporters'
+require 'minitest/spec'
+
+
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   #
